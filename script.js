e3c66900fe80ab1a309e9d3071ce87a02c2a111f
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2020-02-06 16:47:57 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function readyFuntions{
+    console.log("readyFuntions ran")
+}
+
+//function to read number
+//$(.getDogs).on('click', 'input', function(event){}
+//get rid of current pictures
+//call API function
+
+//function to call API
+//fetch('https://dog.ceo/api/breeds/image/random')
+//add API result to an array
+//loop until X where x is read number
+//call display function
+
+//function to display pictures
+//add section $(dogDisplay).append
+//`<img src="${responseJson.message}" class="results-img">`
+//loop until X, where X is array length 
+
+$(readyFuntions);
